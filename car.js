fb6bef3af0e9055c4b416f832bc80215947d705b
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2020-05-23 07:12:13 +0530

    Car-website

---
+++ car.js
@@ -0,0 +1,16 @@
+//var login = prompt("Please enter your name\n");
+
+//alert(login + "\nGood Morning, Welcome to the Website");
+
+/*var a = confirm("Do you really want to leave the page?\n");
+if (a == true) {
+    alert("Thanks for visiting\n");
+} else {
+    alert("Thanks for staying witth us\n");
+}*/
+
+function f() {
+    alert("Thanks for Login\n");
+}
+
+//document.write(login);
